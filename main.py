cc3feb64339c4655bbedaf63e3f167f5d10491d4
Hamza Haque <hamzahaque30@gmail.com>
2022-04-14 12:37:04 -0500

    updated code

--- main.py
+++ main.py
@@ -14,6 +14,17 @@ def return_total_comments():
             
     return sum
 
+def save_files():
+    # save the fake and real comments lists as JSON files
+    with open("fake_comments.json", mode="w") as fake_comments_file:
+        fake_comments_json = json.dumps(fake_comments)
+        fake_comments_file.write(fake_comments_json)
+
+    with open("real_comments.json", mode="w") as real_comments_file:
+        real_comments_json = json.dumps(real_comments)
+        real_comments_file.write(real_comments_json)
+
+
 # Lists that will be holding dictionaries.
 fake_comments = []
 real_comments = []
@@ -37,7 +48,9 @@ with open("comments.json", mode="r", encoding="utf8") as comments:
         answered = False
         while answered is False:
             fake_or_real = input("Is this a fake comment (Y/N): ").lower()
-            if (fake_or_real != "y") and (fake_or_real != "n"):
+            if fake_or_real == "save":
+                answered = True
+            elif (fake_or_real != "y") and (fake_or_real != "n"):
                 answered = False
             else:
                 answered = True
@@ -46,12 +59,17 @@ with open("comments.json", mode="r", encoding="utf8") as comments:
         if fake_or_real == "y":
             # If it's fake, add it to the fake_comments list
             fake_comments.append({"username": comment_user, "comment": comment})
-        else:
+        elif fake_or_real == "n":
             # If it's real, add it to the real_comments list
             real_comments.append({"username": comment_user, "comment": comment})
+            
         
         iteration += 1
         print(f"You have done {iteration} comments so far. You have {return_total_comments() - iteration} comments left. \n\n")
+        print("------------------------------------")
+        
+        # save_files()
+        # print("A fake comment and real comment file has been saved. You may continue.\n")
         
         # Loop through elements in the replies of the comment
         for reply_dict in comment_replies:
@@ -74,23 +92,23 @@ with open("comments.json", mode="r", encoding="utf8") as comments:
             if fake_or_real == "y":
                 # If it's fake, add it to the fake_comments list
                 fake_comments.append({"username": reply_user, "comment": reply})
-            else:
+            elif fake_or_real == "n":
                 # If it's real, add it to the real_comments list
                 real_comments.append({"username": reply_user, "comment": reply})
                 
             iteration += 1
                 
             print(f"You have done {iteration} comments so far. You have {return_total_comments() - iteration} comments left. \n\n")
+            print("--------------------------------------------")
+            
+            # save_files()
+            # print("A fake comment and real comment file has been saved. You may continue.\n")
 
 ## Print the fake and real comments list     
 # print(f"Fake Comments list:\n{fake_comments}\n")
 # print(f"Real Comments list:\n{real_comments}\n")
 
-# save the fake and real comments lists as JSON files
-with open("fake_comments.json", mode="w") as fake_comments_file:
-    fake_comments_json = json.dumps(fake_comments)
-    fake_comments_file.write(fake_comments_json)
 
-with open("real_comments.json", mode="w") as real_comments_file:
-    real_comments_json = json.dumps(real_comments)
-    real_comments_file.write(real_comments_json)
+    
+save_files()
+print("A fake comment and real comment file has been saved.\n")
